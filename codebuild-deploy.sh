a24939ffba2e736ba06a75b121f4d5e9fa9e5c43
Adam Keller <keladam@amazon.com>
2020-10-05 17:10:30 +0000

    Making the move from master to main

--- codebuild-deploy.sh
+++ codebuild-deploy.sh
@@ -9,7 +9,7 @@ set +vx
 
 PRIMARY_BUCKET=$(echo $PRIMARY_BUCKET | cut -f1,2 -d.)
 
-if [ "$CODEBUILD_GIT_CLEAN_BRANCH" = "master" ] ; then
+if [ "$CODEBUILD_GIT_CLEAN_BRANCH" = "main" ] ; then
   aws s3 sync public/ s3://${PRIMARY_BUCKET}/ --cache-control 'max-age=3600, public' --exclude '*' --include '*.html' --include '*.xml';
   aws s3 sync public/ s3://${PRIMARY_BUCKET}/ --cache-control 'max-age=86400, public' --exclude '*.html' --exclude '*.xml';
   aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT} --paths /\*
